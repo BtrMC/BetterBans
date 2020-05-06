c8afd3c15029866013468c647632bf781e2a9dc0
razod <masonboulier@gmail.com>
2020-05-06 18:00:50 -0400

    setup

--- API/index.js
+++ API/index.js
@@ -1,1 +1,11 @@
 const fs = require('fs');
+const express = require('express');
+var app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ "what_are_you_looking":"at" })
+});
+
+app.listen(4000, console.log('http://localhost:4000'))
