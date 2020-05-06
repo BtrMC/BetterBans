a50907d1a705f914b5b153c5e0e04bf1c2e5bbc1
razod <masonboulier@gmail.com>
2020-05-06 18:16:58 -0400

    api works

--- API/index.js
+++ API/index.js
@@ -8,4 +8,28 @@ app.get('/', (req, res) => {
     res.json({ "what_are_you_looking":"at" })
 });
 
+app.get('/get_users', (req, res) => {
+    fs.readFile(__dirname + "/store/bans.json", 'utf8', function readFileCallback(err, data){
+        d = JSON.parse(data);
+        res.json(d)
+    });
+});
+
+app.post('/add_user', (req, res) => {
+    var { name, uuid, reason, banned_by } = req.body;
+    if(!name || !uuid || !reason || !banned_by) {
+        return res.json({ error: "Missing arguments!"})
+    }
+    fs.readFile(__dirname + "/store/bans.json", 'utf8', (err, data) => {
+        if (err){
+            console.log(err);
+        } else {
+        obj = JSON.parse(data);
+        obj.push({ banned_user: name, banned_uuid: uuid, ban_reason: reason, banned_by: banned_by }) //add some data
+        json = JSON.stringify(obj);
+        fs.writeFile(__dirname + "/store/bans.json", json, () => {});
+    }});
+        res.json({ success: "true"})
+})
+
 app.listen(4000, console.log('http://localhost:4000'))
