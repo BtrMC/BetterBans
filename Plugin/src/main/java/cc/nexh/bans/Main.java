b721b46f2ea2e78fdd46b167818de48e2fa05888
razod <masonboulier@gmail.com>
2020-05-06 19:14:02 -0400

    plugin + check if player

--- Plugin/src/main/java/cc/nexh/bans/Main.java
+++ Plugin/src/main/java/cc/nexh/bans/Main.java
@@ -1,4 +1,18 @@
 package cc.nexh.bans;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
+
+public class Main extends JavaPlugin {
+    public Main() {
+    }
+    public void onEnable() {
+        loadConfig();
+        // this.getCommand("wh").setExecutor(new Reload());
+        // Bukkit.getServer().getPluginManager().registerEvents(this, this);
+    }
+    private void loadConfig() {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
 }
--- Plugin/src/main/java/cc/nexh/bans/listeners/banListener.java
+++ Plugin/src/main/java/cc/nexh/bans/listeners/banListener.java
@@ -1,4 +1,43 @@
 package cc.nexh.bans.listeners;
 
-public class banListener {
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import cc.nexh.bans.Main;
+import org.json.simple.JSONObject;
+import org.bukkit.ChatColor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import org.json.simple.parser.JSONParser;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.UUID;
+
+import org.json.simple.parser.ParseException;
+
+public class banListener implements Listener {
+    private Main plugin = Main.getPlugin(Main.class);
+    String API_URL = plugin.getConfig().getString("api");
+    @EventHandler
+    public void onPlayerPreLogin(AsyncPlayerPreLoginEvent evt) throws IOException, ParseException {
+        UUID uuid = evt.getUniqueId();
+        URL url = new URL(API_URL);
+        URLConnection connection = url.openConnection();
+        HttpURLConnection httpConn = (HttpURLConnection) connection;
+        BufferedReader in = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        JSONParser parser = new JSONParser();
+        JSONObject json = (JSONObject) parser.parse(response.toString());
+        if(json.get("banned_uuid").toString().contains(uuid.toString())) {
+            evt.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, "§cYou are perm-banned!\n§bReason " + ChatColor.translateAlternateColorCodes('&', json.get("ban_reason").toString()));
+        }
+    }
 }
